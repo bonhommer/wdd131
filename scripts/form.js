36f9456cbfa7c1a8e64a5b19a66dce7dd8a16d93
bonhommer <antoinebonhommerj@gmail.com>
2024-06-06 13:04:37 -0400

    last changes

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,29 @@
 const products = [
-    { id: 1, name: "Product 1" },
-    { id: 2, name: "Product 2" },
-    { id: 3, name: "Product 3" }
-  ];
+  { id: 'fc-1888', name: 'flux capacitor', avgRating: 4.5 },
+  { id: 'fc-2050', name: 'power laces', avgRating: 4.7 },
+  { id: 'fs-1987', name: 'time circuits', avgRating: 3.5 },
+  { id: 'ac-2000', name: 'low voltage reactor', avgRating: 3.9 },
+  { id: 'jj-1969', name: 'warp equalizer', avgRating: 5.0 }
+];
 
-  const productNameSelect = document.getElementById("productName");
+  document.addEventListener("DOMContentLoaded", () => {
+    const productNameSelect = document.getElementById("productName");
 
-  products.forEach(product => {
-    const option = document.createElement("option");
-    option.value = product.name;
-    option.textContent = product.name;
-    productNameSelect.appendChild(option);
-  });
+    products.forEach(product => {
+      const option = document.createElement("option");
+      option.value = product.id;
+      option.textContent = product.name;
+      productNameSelect.appendChild(option);
+    });
+
+    if (window.location.pathname.endsWith('review.html')) {
+      let reviewCount = parseInt(localStorage.getItem('reviewCount'), 10) || 0;
+      reviewCount++;
+      localStorage.setItem('reviewCount', reviewCount);
+      document.getElementById('review-count').textContent = reviewCount;
+  }
+  })
+ 
 
   document.addEventListener("DOMContentLoaded",function() {
     const currentDate = new Date();
