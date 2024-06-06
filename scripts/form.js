c8f5fd59c44ab9f330bf979475d79c2cc29d538d
bonhommer <antoinebonhommerj@gmail.com>
2024-06-05 23:55:51 -0400

    form html

---
+++ scripts/form.js
@@ -0,0 +1,14 @@
+const products = [
+    { id: 1, name: "Product 1" },
+    { id: 2, name: "Product 2" },
+    { id: 3, name: "Product 3" }
+  ];
+
+  const productNameSelect = document.getElementById("productName");
+
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.name;
+    option.textContent = product.name;
+    productNameSelect.appendChild(option);
+  });
